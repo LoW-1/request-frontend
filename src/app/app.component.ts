c6679641a3c0616760161f1f86cdf3ed5401fc20
lorenz <lorenzwn@googlemail.com>
2020-11-29 00:29:58 +0100

    http requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'request-frontend';
+
+
+  postData = {
+    number: 1234
+  }
+
+  url = 'https://93cb301c-7cc7-44a6-b019-22037a39dc7e.mock.pstmn.io/stuff/'
+  url2 = 'https://93cb301c-7cc7-44a6-b019-22037a39dc7e.mock.pstmn.io/post/'
+  json_response;
+
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit() {
+
+        this.http.get(this.url).subscribe(res => {
+          console.log(JSON.stringify(res));
+        })
+
+
+        this.http.post(this.url2, this.postData).subscribe(res => {
+          console.log(JSON.stringify(res));
+        })
+
+  };
+
 }
